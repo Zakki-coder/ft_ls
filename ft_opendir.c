d357f3f2f46b585c1e727767669e27fbf5d84000
jniemine <jniemine@student.hive.fi>
2022-06-21 18:32:05 +0300

    Stats almost working

---
+++ ft_opendir.c
@@ -0,0 +1,171 @@
+#include <dirent.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include "./libft/libft.h"
+#include "/Users/jniemine/Workspace/ft_ls/libft/ft_printf/includes/ft_printf.h"
+
+typedef struct stat t_stat;
+typedef struct s_file_node t_file_node;
+
+struct s_file_node 
+{
+	t_stat stat;
+	t_stat lstat;
+	char *path; 
+	t_file_node *next;
+};
+
+//     struct stat { /* when _DARWIN_FEATURE_64_BIT_INODE is NOT defined */
+//         dev_t    st_dev;    /* device inode resides on */
+//         ino_t    st_ino;    /* inode's number */
+//         mode_t   st_mode;   /* inode protection mode */
+//         nlink_t  st_nlink;  /* number of hard links to the file */
+//         uid_t    st_uid;    /* user-id of owner */
+//         gid_t    st_gid;    /* group-id of owner */
+//         dev_t    st_rdev;   /* device type, for special file inode */
+//         struct timespec st_atimespec;  /* time of last access */
+//         struct timespec st_mtimespec;  /* time of last data modification */
+//         struct timespec st_ctimespec;  /* time of last file status change */
+//         off_t    st_size;   /* file size, in bytes */
+//         quad_t   st_blocks; /* blocks allocated for file */
+//         u_long   st_blksize;/* optimal file sys I/O ops blocksize */
+//         u_long   st_flags;  /* user defined flags for file */
+//         u_long   st_gen;    /* file generation number */
+//     };
+
+void print_permissions(unsigned int st_mode, struct dirent *dp)
+{
+	char permissions[11];
+
+	ft_memset(permissions, '-', 10);
+	permissions[10] = '\0';
+	if (dp->d_type & DT_DIR)
+		permissions[0] = 'd';
+	if (st_mode & S_IRUSR)
+		permissions[1] = 'r';
+	if (st_mode & S_IWUSR)
+		permissions[2] = 'w';
+	if (st_mode & S_IXUSR)
+		permissions[3] = 'x';
+	if (st_mode & S_IRGRP)
+		permissions[4] = 'r';
+	if (st_mode & S_IWGRP)
+		permissions[5] = 'w';
+	if (st_mode & S_IXGRP)
+		permissions[6] = 'x';
+	if (st_mode & S_IROTH)
+		permissions[7] = 'r';
+	if (st_mode & S_IWOTH)
+		permissions[8] = 'w';
+	if (st_mode & S_IXOTH)
+		permissions[9] = 'x';
+	ft_printf("%-12s", permissions);
+}
+
+void print_time(t_stat *stat)
+{
+	char *time;
+	char output[27];
+
+	ft_bzero(output, 27);
+	time = ctime(&stat->st_mtimespec.tv_sec);
+	time += 8;
+	ft_strncpy(output, time, 3);
+	time -= 4;
+	ft_strncat(output, time, 4);
+	time += 7;
+	ft_strncat(output, time, 5);
+	ft_strncat(output, " ", 1);
+	ft_printf("%*s", ft_strlen(output) + 1, output);
+}
+
+/* TODO: We need to know the longest number of links and longest size beforehand to align correctly. Also the path/filename column should show links.
+void print_stat(t_stat *stat, struct dirent *dp)
+{
+//	printf("%d\n", stat->st_dev);
+//	printf("%llu\n", stat->st_ino);
+	print_permissions(stat->st_mode, dp);
+	ft_printf("%-*u", ft_strlen(ft_itoa(stat->st_nlink)) + 1, stat->st_nlink);
+//	ft_printf("%u\n", stat->st_uid);
+	ft_printf("%-*s", ft_strlen(getpwuid(stat->st_uid)->pw_name) + 2, getpwuid(stat->st_uid)->pw_name);
+//	ft_printf("%u\n", stat->st_gid);
+	ft_printf("%s", getgrgid(stat->st_gid)->gr_name);
+//	ft_printf("%u\n", stat->st_rdev);
+	ft_printf("%*llu", ft_strlen(ft_itoa(stat->st_size)) + 2, stat->st_size); //itoa takes long long st_size is ull, do something? Or not?
+	//         struct timespec st_ctimespec;  /* time of last file status change */
+	print_time(stat);
+	ft_printf("%s\n", dp->d_name);
+//	printf("%ld\n", stat->st_mtimespec.tv_nsec);
+//	printf("%ld\n", stat->st_mtimespec.tv_sec);
+//	printf("%llu\n", stat->st_blocks);
+//	printf("%d\n", stat->st_blksize);
+//	printf("%u\n", stat->st_flags);
+//	printf("%u\n", stat->st_gen);
+}
+//int stat(const char *restrict path, struct stat *restrict buf)
+void read_stat(char *restrict path)
+{}
+
+//             ino_t      d_fileno;     /* file number of entry */
+//             __uint64_t d_seekoff;    /* seek offset (optional, used by servers) */
+//             __uint16_t d_reclen;     /* length of this record */
+//             __uint16_t d_namlen;     /* length of string in d_name */
+//             __uint8_t  d_type;       /* file type, see below */
+//             char    d_name[1024];    /* name must be no longer than this */
+//     };
+
+//     typedef struct _dirdesc {
+//             int     __dd_fd;      /* file descriptor associated with directory */
+//             long    __dd_loc;     /* offset in current buffer */
+//             long    __dd_size;    /* amount of data returned by getdirentries */
+//             char    *__dd_buf;    /* data buffer */
+//             int     __dd_len;     /* size of data buffer */
+//             long    __dd_seek;    /* magic cookie returned by getdirentries */
+//             long    __dd_rewind;  /* magic cookie for rewinding */
+//             int     __dd_flags;   /* flags for readdir */
+//             pthread_mutex_t __dd_lock; /* for thread locking */
+//             struct _telldir *__dd_td; /* telldir position recording */
+//     } DIR;
+
+//int stat(const char *restrict path, struct stat *restrict buf)
+int main(void)
+{
+	DIR *dirp;
+	t_file_node *head;
+	struct dirent *dp;
+	struct dirent *dp2;
+	struct stat buf[1024];
+	char *fd;
+	char path[1024] = "./";
+	int len;
+	int ret = 5;
+
+	dirp = opendir(path);
+	dp = readdir(dirp);
+	len = ft_strlen(path) + ft_strlen(dp->d_name);
+	head = (t_file_node *)malloc(sizeof(t_file_node));
+	head->path = (char *)ft_memalloc(len + 2);
+		if (!head)
+			;//bla bla
+	ft_strcat(head->path, path);
+	ft_strcat(head->path, dp->d_name);
+//	ft_strcat(head->path, "/");
+	ret = stat(head->path, &head->stat);
+	lstat(head->path, &head->lstat);
+	print_stat(&head->stat, dp);
+//	print_stat(&head->lstat, dp);
+	/*
+	while (dp)
+	{
+		dp = readdir(dirp);
+	}
+	*/
+	return (0);
+}
