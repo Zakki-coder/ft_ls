30aeab2d797d4dff560f15aed892f761a5046d90
Jakke Nieminen <jniemine@c1r3p8.hive.fi>
2022-09-14 19:40:58 +0300

    Norm

--- parser.c
+++ parser.c
@@ -6,67 +6,67 @@
 /*   By: jniemine <jniemine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 17:58:51 by jniemine          #+#    #+#             */
-/*   Updated: 2022/09/13 20:48:12 by jniemine         ###   ########.fr       */
+/*   Updated: 2022/09/14 19:40:35 by jniemine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_ls.h"
 
-void print_usage(void)
+static void	print_usage(void)
 {
 	ft_printf("usage: ls [%s] [file ...]\n", ALLOWED_OPTIONS);
 	exit (-1);
 }
 
-int set_flags(char c, unsigned int *flags)
+static void	print_flag_errors(char c)
 {
-	if(c == 'l')
+	write(STDERR_FILENO, "ft_ls: illegal option -- ", 25);
+	write(STDERR_FILENO, &c, 1);
+	write(STDERR_FILENO, "\n", 1);
+	print_usage();
+}
+
+static int	set_flags(char c, unsigned int *flags)
+{
+	if (c == 'l')
 	{
 		if (*flags & ONE_COLUMN)
 			*flags ^= ONE_COLUMN;
 		*flags |= LONG_LST;
 	}
-	else if(c == 'R')
-		*flags |= RECURSIVE; 
-	else if(c == 'r')
-		*flags |= REVERSE_ORDER; 
-	else if(c == 'a')
-		*flags |= ALL; 
-	else if(c == 't')
-		*flags |= TIME_ORDER; 
-	else if(c == '1')
+	else if (c == 'R')
+		*flags |= RECURSIVE;
+	else if (c == 'r')
+		*flags |= REVERSE_ORDER;
+	else if (c == 'a')
+		*flags |= ALL;
+	else if (c == 't')
+		*flags |= TIME_ORDER;
+	else if (c == '1')
 	{
 		if (*flags & LONG_LST)
 			*flags ^= LONG_LST;
 		*flags |= ONE_COLUMN;
 	}
-	else if(c == '@')
+	else if (c == '@')
 		*flags |= EXT_ATTR;
-//	else if(c == 'e')
-//		*flags |= ACL;
 	else
-	{
-		write(STDERR_FILENO, "ft_ls: illegal option -- ", 25);
-		write(STDERR_FILENO, &c, 1);
-		write(STDERR_FILENO, "\n", 1);
-		print_usage();
-	}
+		print_flag_errors(c);
 	return (1);
 }
 
-//TODO Print illegal option and usage if illegal option.
-int ls_get_flags(int argc, char **argv, unsigned int *flags)
+int	ls_get_flags(int argc, char **argv, unsigned int *flags)
 {
 	int		i;
 	int		k;
 
 	i = 1;
-	while(i < argc && argv[i][0] == '-')
+	while (i < argc && argv[i][0] == '-')
 	{
 		k = 0;
 		if (argv[i][1] == '\0' || argv[i][1] == ' ')
 			return (i);
-		if ((argv[i][1] == '-' && ft_strlen(argv[i]) == 2) )
+		if (argv[i][1] == '-' && ft_strlen(argv[i]) == 2)
 			return (++i);
 		while (argv[i][++k])
 			set_flags(argv[i][k], flags);
@@ -74,14 +74,3 @@ int ls_get_flags(int argc, char **argv, unsigned int *flags)
 	}
 	return (i);
 }
-
-
-/*
-void parser(int argc, char **argv, unsigned int flags)
-{
-	if(argc > 1)
-	{
-		return(get_flags(argc, argv);
-	}
-}
-*/
